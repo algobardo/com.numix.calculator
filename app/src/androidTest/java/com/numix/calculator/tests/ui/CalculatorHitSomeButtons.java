5b3b8c6f6ec69449883765b706f5ba8f9e6a2df7
christofferqa <christofferqa@gmail.com>
2015-01-29 15:37:01 +0100

    Working test

--- app/src/androidTest/java/com/numix/calculator/tests/ui/CalculatorHitSomeButtons.java
+++ app/src/androidTest/java/com/numix/calculator/tests/ui/CalculatorHitSomeButtons.java
@@ -2,15 +2,22 @@ package com.numix.calculator.test.ui;
 
 import android.app.Activity;
 import android.test.ActivityInstrumentationTestCase2;
+import android.view.View;
 
 import com.numix.calculator.Calculator;
+import com.numix.calculator.view.CalculatorDisplay;
 import com.numix.calculator.R;
+
+import android.support.test.espresso.matcher.BoundedMatcher;
 
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
-import static android.support.test.espresso.matcher.ViewMatchers.withText;
+
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+
 import static org.hamcrest.Matchers.is;
 
 public class CalculatorHitSomeButtons extends ActivityInstrumentationTestCase2<Calculator> {
@@ -38,6 +45,21 @@ public class CalculatorHitSomeButtons extends ActivityInstrumentationTestCase2<C
         onView(withId(R.id.plus)).perform(click());
         onView(withId(R.id.digit2)).perform(click());
         onView(withId(R.id.equal)).perform(click());
-        onView(withId(R.id.display)).check(matches(withText(is("4"))));
+        onView(withId(R.id.display)).check(matches(withCalculatorDisplayText(is("4"))));
+    }
+
+    public static Matcher<View> withCalculatorDisplayText(final Matcher<String> stringMatcher) {
+        return new BoundedMatcher<View, CalculatorDisplay>(CalculatorDisplay.class) {
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("with calculator display text: ");
+                stringMatcher.describeTo(description);
+            }
+
+            @Override
+            public boolean matchesSafely(CalculatorDisplay calculatorDisplay) {
+                return stringMatcher.matches(calculatorDisplay.getText().toString());
+            }
+        };
     }
 }
