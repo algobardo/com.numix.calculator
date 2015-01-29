ec0cb5ee2ad62a5e637306074a5426e4b40a5f39
christofferqa <christofferqa@gmail.com>
2015-01-29 17:13:47 +0100

    Final

--- app/src/androidTest/java/com/numix/calculator/tests/ui/CalculatorHitSomeButtons.java
+++ app/src/androidTest/java/com/numix/calculator/tests/ui/CalculatorHitSomeButtons.java
@@ -8,12 +8,12 @@ import com.numix.calculator.Calculator;
 import com.numix.calculator.view.CalculatorDisplay;
 import com.numix.calculator.R;
 
-import android.support.test.espresso.matcher.BoundedMatcher;
+import com.google.android.apps.common.testing.ui.espresso.matcher.BoundedMatcher;
 
-import static android.support.test.espresso.Espresso.onView;
-import static android.support.test.espresso.action.ViewActions.click;
-import static android.support.test.espresso.assertion.ViewAssertions.matches;
-import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static com.google.android.apps.common.testing.ui.espresso.Espresso.onView;
+import static com.google.android.apps.common.testing.ui.espresso.action.ViewActions.click;
+import static com.google.android.apps.common.testing.ui.espresso.assertion.ViewAssertions.matches;
+import static com.google.android.apps.common.testing.ui.espresso.matcher.ViewMatchers.withId;
 
 import org.hamcrest.Description;
 import org.hamcrest.Matcher;
